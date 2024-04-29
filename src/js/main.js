80aed44414a38326087b77824924f6da2cdbe87b
Slava Khripushin <slava.cpa@yandex.ru>
2024-04-29 19:25:20 +0700

    feat: section "noble"

--- src/js/main.js
+++ src/js/main.js
@@ -27,4 +27,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const maskedElements = document.querySelectorAll('.masked');
   maskedElements.forEach((item) => new IMask(item, maskOptions));
+
+  const nobleSlider = new Swiper('.noble-slider', {
+    spaceBetween: 25,
+    speed: 500,
+    navigation: {
+      nextEl: '.noble-arrow__next',
+      prevEl: '.noble-arrow__prev',
+    },
+    on: {
+      init: function () {
+        updateNobleSliderCounter(this);
+      },
+      slideChange: function () {
+        updateNobleSliderCounter(this);
+      },
+    },
+  });
+
+  function updateNobleSliderCounter(swiper) {
+    const currentIndex = swiper.realIndex + 1;
+    const totalSlides = swiper.slides.length;
+
+    document.querySelector('.noble-count .current').textContent = currentIndex.toString().padStart(2, '0');
+    document.querySelector('.noble-count .total').textContent = totalSlides.toString().padStart(2, '0');
+  }
 });
